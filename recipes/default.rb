cf8f129c146215d424d76fd18e75420bfc99dea0
Guillaume Hain <zedtux@zedroot.org>
2020-03-17 09:55:50 +0100

    Fixes the cookbook execution and rubocop offenses

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,6 +1,17 @@
-ruby_block "Save node attributes" do
+# frozen_string_literal: true
+
+ruby_block 'Save node attributes' do
+  action :run
   block do
-  	parent = node['export-node']['location']
-    IO.write(File.join(parent, "chef_node.json"), node.to_json) if Dir::exist?(parent)
+    parent = node['export-node']['location']
+
+    if Dir.exist?(parent)
+      output_path = File.join(parent, 'chef_node.json')
+      Chef::Log.info "Writing node attributes to #{output_path} ..."
+      IO.write(output_path, node.to_json)
+    else
+      Chef::Log.warn 'The node attributes have not been exported as the ' \
+                     "output folder #{parent} doesn't exists."
+    end
   end
 end
